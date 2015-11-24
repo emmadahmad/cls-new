d16c849d565bbf49147ec81c38967f6f65b368aa
Emmad Ahmad <emmad.ahmad@hotmail.com>
2015-11-24 09:56:27 +0500

    FIrst Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+cls_new
+=======
+
+A Symfony project created on August 8, 2015, 2:06 pm.
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+class User extends BaseUser
+{
+}
